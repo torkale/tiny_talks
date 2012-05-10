50ef798737d46cf558bdb2b943ec432a8c439e28
Tor Ivry <tor@sygen.co>
2012-05-10 23:55:36 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinyTalks::Application.config.secret_token = 'bb07bdabca7cbd8c978b5c1b246743750acfedcb88816ab731a46e16cae47a893d0271304a487947667228667857365549c6856511648b864c96d8bc73b2d14a'
